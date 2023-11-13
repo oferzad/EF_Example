82269baada0999420b01df1911811b9ef653163f
Ofer Zadikario <ofer.zadikario@gmail.com>
2023-11-13 10:43:22 +0200

    Add project files.

---
+++ EF_Example/Models/HighScoresDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Example.Models;
+
+public partial class HighScoresDbContext : DbContext
+{
+    public HighScoresDbContext()
+    {
+    }
+
+    public HighScoresDbContext(DbContextOptions<HighScoresDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Game> Games { get; set; }
+
+    public virtual DbSet<HighScore> HighScores { get; set; }
+
+    public virtual DbSet<Player> Players { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database=GameHighScores; Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.Property(e => e.Name).IsFixedLength();
+        });
+
+        modelBuilder.Entity<HighScore>(entity =>
+        {
+            entity.HasKey(e => new { e.GameId, e.PlayerId }).HasName("PK_HighScore");
+
+            entity.HasOne(d => d.Game).WithMany(p => p.HighScores)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_HighScore_Game");
+
+            entity.HasOne(d => d.Player).WithMany(p => p.HighScores)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_HighScore_Player");
+        });
+
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.Property(e => e.Name).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_Example/Program.cs
@@ -0,0 +1,190 @@
+﻿using EF_Example.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EF_Example
+{
+    internal class Program
+    {
+        //SHow change tracker for debugging and learning purpuses
+        static void ShowChangeTrackerObjects(HighScoresDbContext db)
+        {
+            db.ChangeTracker.DetectChanges();
+            Console.WriteLine(db.ChangeTracker.DebugView.LongView);
+        }
+
+        //Deleting objects in database
+        static void DeleteObjectsUsingChangeTracker()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player p = new Player
+            {
+                Name = "Tobe Deleted",
+                BirthYear = 2000,
+            };
+
+            db.Entry(p).State = EntityState.Added;
+            db.SaveChanges();
+
+            //Now delete the object
+            db.Entry(p).State = EntityState.Deleted;
+            db.SaveChanges();
+
+
+        }
+
+        //Updating existing objects in database
+        static void UpdateObjectsUsingChangeTracker()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player? p = db.Players.Where(pp => pp.Name == "Marom").FirstOrDefault();
+            
+            if (p != null)
+            {
+                p.Name = "Hadas";
+                //The belolw state is done automatically as p is already tracked!
+                //db.Entry(p).State = EntityState.Modified;
+                ShowChangeTrackerObjects(db);
+                db.SaveChanges();
+            }
+                
+        }
+
+        //Adding new nested objects to database
+        static void AddNestedObjectsUsingAddOperation()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player p = new Player
+            {
+                Name = "Nested PLayer Object",
+                BirthYear = 200,
+            };
+            p.HighScores.Add(new HighScore()
+            {
+                GameId = 1,
+                HighScore1 = 1000,
+            });
+
+            db.Players.Add(p);
+            ShowChangeTrackerObjects(db);
+            db.SaveChanges();
+            Console.WriteLine(p.PlayerId);
+        }
+
+        static void AddNestedObjectsUsingChangeTracker()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player p = new Player
+            {
+                Name = "Nested PLayer Object",
+                BirthYear = 200,
+            };
+            p.HighScores.Add(new HighScore()
+            {
+                GameId = 1,
+                HighScore1 = 1000,
+            });
+
+            //First add the player object and get player id from database
+            db.Entry(p).State = EntityState.Added;
+            db.SaveChanges();
+            //now update all high score objects connected to the player with the generated player id
+            foreach(HighScore h in  p.HighScores)
+            {
+                h.PlayerId = p.PlayerId;
+                db.Entry(h).State = EntityState.Added;
+            }
+            //save changes again!
+            db.SaveChanges();
+
+        }
+
+
+        //Adding new objects to database
+        static void AddObjectsUsingChangeTracker()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player p = new Player
+            {
+                Name = "Marom",
+                BirthYear = 200,
+            };
+
+            db.Entry(p).State = EntityState.Added;
+            ShowChangeTrackerObjects(db);
+            db.SaveChanges();
+            Console.WriteLine(p.PlayerId);
+        }
+
+        static void AddObjectsUsingAddOperation()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            Player p = new Player
+            {
+                Name = "Marom",
+                BirthYear = 200,
+            };
+
+            db.Players.Add(p);
+            ShowChangeTrackerObjects(db);
+            db.SaveChanges();
+            Console.WriteLine(p.PlayerId);
+        }
+
+
+        //Show how to extrapulate navigation with eager loading
+        static void ReadWithEagerLoading()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            List<Player> players = db.Players.
+                                      Include(p => p.HighScores).
+                                      ThenInclude(h => h.Game).ToList();
+
+            foreach (Player p in players)
+            {
+                Console.WriteLine(p.Name);
+                Console.WriteLine(p.BirthYear);
+                Console.WriteLine($"This PLayer has {p.HighScores.Count} High Scores:");
+                foreach (HighScore highScore in p.HighScores)
+                {
+                    Console.WriteLine($"In Game: {highScore.Game.Name} High Score: {highScore.HighScore1}");
+                }
+            }
+        }
+
+        //Show how to extrapulate navigation properties on demand
+        static void ReadWithoutEagerLoading()
+        {
+            HighScoresDbContext db = new HighScoresDbContext();
+
+            List<Player> players = db.Players.ToList();
+
+            foreach (Player p in players)
+            {
+                Console.WriteLine(p.Name);
+                Console.WriteLine(p.BirthYear);
+                List<HighScore> highScores = db.HighScores.Where(h => h.PlayerId == p.PlayerId).ToList();
+                Console.WriteLine($"This PLayer has {highScores.Count} High Scores:");
+                foreach (HighScore highScore in highScores)
+                {
+                    Game? g = db.Games.Where(gg => gg.GameId == highScore.GameId).FirstOrDefault();
+                    Console.WriteLine($"In Game: {g.Name} High Score: {highScore.HighScore1}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            //scaffold-DbContext "Server = (localdb)\MSSQLLocalDB; Database=GameHighScores; Trusted_Connection=True; TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context HighScoresDbContext -DataAnnotations -force
+            AddObjectsUsingAddOperation();
+        }
+    }
+}
